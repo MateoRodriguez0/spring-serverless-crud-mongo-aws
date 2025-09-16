727b1ef1e4b7ac9329410ca59cb2bfcbe14f6384
Mateo Rodriguez <mateo204r@gmail.com>
2025-09-16 14:01:37 -0500

    chore: create base project with initial dependencies

---
+++ src/main/java/com/example/SpringServerlessCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerlessCrudTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerlessCrudTutorialApplication.class, args);
+    }
+
+}
